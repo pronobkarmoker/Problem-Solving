c04fc0b87f970027466a739575fb7203f7b137ee
Pronob <pronobkarmoker1@gmail.com>
2023-09-04 21:51:00 +0600

    04-09-2023

---
+++ CodeForces/A. Word Capitalization.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    char word[1001];
+    gets(word);
+    if (word[0] >= 'a' && word[0] <= 'z')
+    {
+        word[0] = word[0] - 32;
+        printf(word);
+    }
+    else
+    {
+        printf(word);
+    }
+    return 0;
+}
