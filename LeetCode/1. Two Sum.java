8dc2fd79eddb8b638ea02950b983ab1cb198d6c0
Pronob <pronobkarmoker1@gmail.com>
2023-09-14 12:37:47 +0600

    LeetCode(14-09-23)

---
+++ LeetCode/1. Two Sum.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+public class 1. Two Sum{
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0 ;i< n; i++){
+            for(int j= 0; j<n;j++){
+                if(target ==nums.get(i)+nums.get(j)){
+                    return nums[i,j];
+                }
+            }
+        }
+    }
+}
+
+}
