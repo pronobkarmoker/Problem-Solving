ba62999cc33efe174ba34971a5388c59ebd94702
Pronob <pronobkarmoker1@gmail.com>
2023-09-14 13:08:37 +0600

    LeetCode(14-09-23)

---
+++ LeetCode/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+
+        Arrays.sort(strs);
+
+        char[] first = strs[0].toCharArray();
+        char[] last = strs[strs.length - 1].toCharArray();
+
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] != last[i])
+                break;
+            result.append(first[i]);
+        }
+
+        return result.toString();
+    }
+}
---
+++ LeetCode/TwoSum.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class TwoSum {
+    class Solution {
+    /**
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int twoSum(int[] nums, int target) {
+        int n;
+        for(int i = 0 ;i< n; i++){
+            for(int j= 0; j<n;j++){
+                if(target ==nums.length+nums.length){
+                    return nums[i,j];
+                }
+            }
+        }
+    }
+    }
+
+}
