922210a1a46c570bb6414809b9f12ad93dee1642
Pronob <pronobkarmoker1@gmail.com>
2023-09-15 00:49:51 +0600

    15-09-2023

---
+++ LeetCode/ValidParentheses.java
@@ -0,0 +1,39 @@
+package LeetCode;
+
+import java.util.Stack;
+
+class ValidParentheses {
+    public char getVal(char ch) {
+        switch (ch) {
+            case ']':
+                return '[';
+            case '}':
+                return '{';
+            case ')':
+                return '(';
+            default:
+                return ' ';
+        }
+    }
+
+    public boolean isValid(String s) {
+        String open = "({[";
+        String close = "]})";
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (open.indexOf(ch) != -1) {
+                st.push(ch);
+            } else {
+                if (st.size() == 0)
+                    return false;
+                char tmp = st.pop();
+                if (getVal(ch) != tmp) {
+                    return false;
+                }
+            }
+
+        }
+        return st.size() == 0;
+    }
+}
