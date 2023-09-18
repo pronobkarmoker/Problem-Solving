d99eb7317a0c48a6bba36a3eb91a3294d34baf59
Pronob <pronobkarmoker1@gmail.com>
2023-09-19 00:55:15 +0600

    18-9-23

---
+++ LeetCode/PlusOne.java
@@ -0,0 +1,19 @@
+
+public class PlusOne {
+
+    class Solution {
+        public int[] plusOne(int[] digits) {
+            for (int i = digits.length - 1; i >= 0; i--) {
+                if (digits[i] < 9) {
+                    digits[i]++;
+                    return digits;
+                } else {
+                    digits[i] = 0;
+                }
+            }
+            int[] newNum = new int[digits.length + 1];
+            newNum[0] = 1;
+            return newNum;
+        }
+    }
+}
--- LeetCode/RemoveDuplicatefromSortedArray.java
+++ LeetCode/RemoveDuplicatefromSortedArray.java
@@ -1,4 +1,3 @@
-package LeetCode;
 
 public class RemoveDuplicatefromSortedArray {
     class Solution {
