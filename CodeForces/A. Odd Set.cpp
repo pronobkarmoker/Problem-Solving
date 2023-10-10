11f5fe80509b9d461885245d0785fc224bc1bc58
Pronob <pronobkarmoker1@gmail.com>
2023-10-10 22:03:35 +0600

    10-10-2023

---
+++ CodeForces/A. Odd Set.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[2 * n];
+        int count = 0;
+        for (int i = 0; i < 2 * n; i++)
+        {
+            cin >> a[i];
+            if (a[i] % 2 != 0)
+            {
+                count++;
+            }
+        }
+        if (count == n)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
