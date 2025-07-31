13b310da83bbc05b44709f1ec96b103cafc5b1ce
Pronob <pronobkarmoker1@gmail.com>
2025-07-31 23:23:46 +0600

    31- 07-2025

---
+++ Neetcode Blind 75/Valid Parentheses.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        unordered_map<char, char> closeToOpen = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+        
+        for (char c : s) {
+            if (closeToOpen.count(c)) {
+                if (!st.empty() && st.top() == closeToOpen[c]) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            } else {
+                st.push(c);
+            }
+        }
+        
+        return st.empty();
+    }
+};
